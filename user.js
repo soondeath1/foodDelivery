43530516bfe572e3e8b976c738150b98c385f06d
soondeath1 <seungchan0805@naver.com>
2025-05-21 13:54:27 +0900

    Update user.js

--- user.js
+++ user.js
@@ -1,8 +1,20 @@
 // user.js
+
 class User {
-  constructor(name) {
-    this.name = 박승찬;
-  }
+    constructor(name, email) {
+        this.name = 박승찬;
+        this.email = soondeath1@sungkyul.ac.kr;
+    }
+
+    // 알림 보내기
+    notify(message) {
+        console.log(`사용자 ${this.name} (${this.email})에게 알림: ${message}`);
+    }
+
+    // 사용자 정보 출력
+    getUserInfo() {
+        return `사용자: ${this.name}, 이메일: ${this.email}`;
+    }
 }
 
 module.exports = User;
