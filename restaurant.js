319a26cac23fe46d79d0b5b4bb01dd847acb1e9b
soondeath1 <seungchan0805@naver.com>
2025-05-21 13:56:31 +0900

    Update restaurant.js

--- restaurant.js
+++ restaurant.js
@@ -1,15 +1,36 @@
 // restaurant.js
-class Restaurant {
-  checkAvailability(menuItem) {
-    // 음식 재고 확인
-    console.log(`${menuItem} 재고 확인`);
-    return true;  // 여기서는 항상 재고 있음으로 가정
-  }
 
-  receiveOrder(menuItem) {
-    // 주문 접수
-    console.log(`${menuItem} 주문 접수 완료`);
-  }
+class Restaurant {
+    constructor(name, menu) {
+        this.name = name;
+        this.menu = menu; // 메뉴는 {아이템명: 수량} 형식으로 저장됩니다.
+    }
+
+    // 재고 확인 함수
+    checkStock(item, quantity) {
+        if (!this.menu[item]) {
+            console.log(`${item}는 메뉴에 없습니다.`);
+            return false;
+        }
+        if (this.menu[item] < quantity) {
+            console.log(`${item}의 재고가 부족합니다.`);
+            return false;
+        }
+        console.log(`${item}의 재고는 충분합니다.`);
+        return true;
+    }
+
+    // 재고 업데이트 함수
+    updateStock(item, quantity) {
+        if (this.menu[item]) {
+            this.menu[item] -= quantity;
+            console.log(`${item}의 재고가 ${quantity}만큼 차감되었습니다.`);
+        } else {
+            console.log(`${item}는 메뉴에 없습니다.`);
+        }
+    }
 }
 
 module.exports = Restaurant;
+
+
